13bdce3be00f18edc29bcc5c762484baedcc2f7c
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2026-01-28 15:05:12 +0000

    Update pxt.json, SceneExtension.ts

---
+++ SceneExtension.ts
@@ -0,0 +1,17 @@
+namespace scene {
+
+    //% blockId=_tileImage 
+    //% block="%img"
+    //% img.fieldEditor="tileset"
+    //% img.fieldOptions.taggedTemplate="img"
+    //% img.fieldOptions.decompileIndirectFixedInstances="true"
+    //% img.fieldOptions.sizes="16,16;32,32;8,8"
+    //% img.fieldOptions.filter="tile"
+    //% weight=10 
+    //% blockNamespace="scene" 
+    //% group="Tiles"
+    export function _tileImage(img: Image) {
+        return img
+    }
+
+}
