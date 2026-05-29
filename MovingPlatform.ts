77490bb3953126614c0d791fa6df7aaac9407b75
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2026-05-29 19:35:00 +0100

    Update MovingPlatform.ts

--- MovingPlatform.ts
+++ MovingPlatform.ts
@@ -61,7 +61,7 @@ namespace tilesAdvanced {
             if (sprite.y < wall.y) {
                 sprite.bottom = wall.top;
                 sprite.vx = wall.vx //makes it follow
-                if (sprite.vy > 0) {
+                if (sprite.vy >= 0) {
                     sprite.vy = 0; // only do if were falling. lets you jump
                 }
             } else {
