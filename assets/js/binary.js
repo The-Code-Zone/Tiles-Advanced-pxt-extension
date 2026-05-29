45a202900b7a675c39c4195f1b121c5a79d0fc7c
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2026-05-29 19:46:22 +0100

    1.2.9

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"/simulator.html","cdnUrl":"/","version":"1.2.8","target":"arcade","targetVersion":"2.0.3","repo":"tomhausen/the-code-zone-tiles-advanced"}
+// meta={"simUrl":"/simulator.html","cdnUrl":"/","version":"1.2.9","target":"arcade","targetVersion":"2.0.3","repo":"tomhausen/the-code-zone-tiles-advanced"}
 // total=1256903 new=5.87% cached=88.47% other=5.67%
 (function (ectx) {
 'use strict';
@@ -429,7 +429,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P184072(s) {
+function _main___P184672(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -831,23 +831,23 @@ switch (step) {
     s.callLocIdx = 63; s.pc = 37; return s.tmp_0;
   case 37:
     r0 = s.retval;
-    globals.MovingPlatform___183674 = (r0);
+    globals.MovingPlatform___184274 = (r0);
     s.tmp_0 = SpriteKind_create__P154356_mk(s);
     s.callLocIdx = 64; s.pc = 38; return s.tmp_0;
   case 38:
     r0 = s.retval;
-    globals.MovingPlatformHitbox___183677 = (r0);
+    globals.MovingPlatformHitbox___184277 = (r0);
     r0 = undefined;
     return leave(s, r0)
   default: oops()
 } } }
-_main___P184072.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/a-star/main.ts","functionName":"<main>","argumentNames":[]}
-_main___P184072.continuations = [  ]
+_main___P184672.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"pxt_modules/a-star/main.ts","functionName":"<main>","argumentNames":[]}
+_main___P184672.continuations = [  ]
 
-function _main___P184072_mk(s) {
+function _main___P184672_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P184072, depth: s.depth + 1,
+        parent: s, fn: _main___P184672, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -42362,7 +42362,7 @@ const sprites_BaseSpriteSayRenderer__C154136_VT = mkVTable({
   },
 });
 
-const breakpoints = setupDebugger(1, ["MovingPlatform___183674","MovingPlatformHitbox___183677"])
+const breakpoints = setupDebugger(1, ["MovingPlatform___184274","MovingPlatformHitbox___184277"])
 
-return _main___P184072
+return _main___P184672
 })
